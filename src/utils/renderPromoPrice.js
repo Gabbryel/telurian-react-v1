36db46e1ef2c264ce40606020eefc6e8159212e2
Gabriel Ursache <gabriel@markov.ro>
2021-03-27 22:22:53 +0200

    solved utc error :))

--- src/utils/renderPromoPrice.js
+++ src/utils/renderPromoPrice.js
@@ -1,7 +1,7 @@
 const renderPrice = (promoPrice, price) => {
   const day = new Date().getUTCDate();
   const hour = new Date().getUTCHours();
-  if ((day === 28 && hour >= 2) || (day === 29 && hour < 2)) {
+  if ((day === 27 && hour >= 22) || (day === 28 && hour < 22)) {
     return promoPrice;
   }
   return price;
